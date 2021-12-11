df188625643d27bfad85f0c7cd04691fe48aabba
Atiq Samtia <atiq@ioitechnologies.com>
2021-12-11 11:16:14 +0500

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_ui_sample/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase UI Sample',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
